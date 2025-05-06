3cf9aa4fe2adc39facbebcc17788a0781c6f769f
Arav PP <aravpp03@gmail.com>
2025-05-06 21:28:54 +0530

    Display Label Created

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,11 @@
+import java.awt.Color;
+
 import javax.swing.*;
 
 public class Calculator {
 	
 	JFrame jf;
+	JLabel displayLabel;
 	
 	public Calculator() {
 		
@@ -10,6 +13,14 @@ public class Calculator {
 		jf.setLayout(null);
 		jf.setSize(500, 550);
 		jf.setLocation(520, 100);
+		
+		displayLabel = new JLabel();
+		displayLabel.setBounds(20, 50, 445, 60);
+		displayLabel.setBackground(Color.LIGHT_GRAY);
+		displayLabel.setOpaque(true);
+		displayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		displayLabel.setForeground(Color.black);
+		jf.add(displayLabel);
 		
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
